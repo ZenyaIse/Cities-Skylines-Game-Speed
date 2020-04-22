93cb8e61bc20ada91d6a579f6bb14594c0829420
ZenyaIse <zenya@zenya.info>
2020-04-22 21:37:56 +0900

    Loans set/reset fix (for compatibility)

--- Source/Loans.cs
+++ Source/Loans.cs
@@ -1,21 +1,20 @@
 ﻿using ColossalFramework;
+using UnityEngine;
 
 namespace GameSpeedMod
 {
     public static class Loans
     {
-        private static int[] m_amount_orig = null;
-        private static int[] m_length_orig = null;
+        private static bool isAlreadySet = false;
 
         public static void SetLoans()
         {
+            if (isAlreadySet) return;
+
             EconomyManager em = Singleton<EconomyManager>.instance;
             GameSpeedManager gs = Singleton<GameSpeedManager>.instance;
 
             if (em.m_properties == null || em.m_properties.m_banks == null) return;
-
-            m_amount_orig = new int[3];
-            m_length_orig = new int[3];
 
             for (int i = 0; i < 3; i++)
             {
@@ -25,32 +24,33 @@ namespace GameSpeedMod
                 int newAmount = oldAmount * gs.Parameters.LoanMultiplier;
                 int newLength = oldLength * (1 + gs.Parameters.LoanMultiplier) / 2; // Halve the effect to prevent too long loan length.
 
-                m_amount_orig[i] = oldAmount;
-                m_length_orig[i] = oldLength;
-
                 em.m_properties.m_banks[i].m_loanOffers[0].m_amount = newAmount;
                 em.m_properties.m_banks[i].m_loanOffers[0].m_length = newLength;
 
                 Logger.Add(em.m_properties.m_banks[i].m_bankName, "amount", oldAmount, newAmount, "length", oldLength, newLength);
             }
+
+            isAlreadySet = true;
         }
 
         public static void ResetLoans()
         {
-            if (m_amount_orig == null || m_length_orig == null) return;
+            if (!isAlreadySet) return;
 
             EconomyManager em = Singleton<EconomyManager>.instance;
+            GameSpeedManager gs = Singleton<GameSpeedManager>.instance;
 
             for (int i = 0; i < 3; i++)
             {
-                em.m_properties.m_banks[i].m_loanOffers[0].m_amount = m_amount_orig[i];
-                em.m_properties.m_banks[i].m_loanOffers[0].m_length = m_length_orig[i];
+                em.m_properties.m_banks[i].m_loanOffers[0].m_amount /= gs.Parameters.LoanMultiplier;
+
+                int value = em.m_properties.m_banks[i].m_loanOffers[0].m_length;
+                em.m_properties.m_banks[i].m_loanOffers[0].m_length = Mathf.RoundToInt(value * 2f / (1 + gs.Parameters.LoanMultiplier));
             }
 
-            m_amount_orig = null;
-            m_length_orig = null;
-
             Logger.Add("Reset loans");
+
+            isAlreadySet = false;
         }
     }
 }
